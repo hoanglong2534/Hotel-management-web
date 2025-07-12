539bdab462117ecec78b5226cb227bc77e2f8f64
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-07-12 12:38:30 +0700

    [fe] - add Welcome (RoomPage)

--- fe/hotel-web/src/App.jsx
+++ fe/hotel-web/src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
+import Room from './pages/Room'
 
 function App() {
 
   return (
     <>
       {/* <Home /> */}
-      <About/>
+      {/* <About/> */}
+      <Room/>
     </>
 
   )
---
+++ fe/hotel-web/src/pages/Room.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Welcome from "../components/Room/Welcome"
+function Room() {
+  return (
+    <>
+    <Header/>
+    <Welcome/>
+    <Footer/>
+    </>
+  )
+}
+export default Room
